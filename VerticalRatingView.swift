4a411289f87b0bcfe22405dd75b34a7129bb5bd2
bofualex <bofu_alex@yahoo.com>
2017-12-12 11:40:02 +0200

    Update VerticalRatingView.swift

--- VerticalRatingView.swift
+++ VerticalRatingView.swift
@@ -1,6 +1,6 @@
 // VerticalRatingView.swift
 //
-// Copyright (c) 2013 Alex Bofu
+// Copyright (c) 2017 Alex Bofu
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
